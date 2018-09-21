1097cb2731dbd563adaccb9a97c023288d4071c9
CesarDSC2 <42946037+CesarDSC2@users.noreply.github.com>
2018-09-21 12:23:57 -0700

    Add files via upload

---
+++ Del1almillon/Del1almillon/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Del1almillon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime time = DateTime.Now;
+
+            double suma = 0;
+            Console.WriteLine("\t°Suma del 1 al 1,000,000°");
+            for (int i = 1; i <= 1000000; i++)
+            {
+                suma = suma + i;
+            }
+            Console.WriteLine("\nLa suma total es: {0}", suma);
+
+            DateTime time2 = DateTime.Now;
+            TimeSpan total = new TimeSpan(time2.Ticks - time.Ticks);
+            Console.Write("\nTiempo de ejecución: " + total.ToString());
+            Console.ReadKey();
+        }
+    }
+}
